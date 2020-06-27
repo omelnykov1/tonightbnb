78ded6f19cf578ab134b6175a47bb68a0a9446ad
omelnykov1 <sashqa.melnikov@gmail.com>
2020-06-27 15:07:43 -0400

    search containers

---
+++ frontend/components/search/search_show_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { fetchSearch } from '../../actions/spot_actions';
+import SearchShow from './search_show';
+
+const mSTP = (state) => ({
+    spots: Object.values(state.entities.spots),
+    search: state.entities.search,
+})
+
+const mDTP = dispatch => ({
+    fetchSearch: (query) => dispatch(fetchSearch(query))
+})
+
+export default connect(mSTP, mDTP)(SearchShow);
